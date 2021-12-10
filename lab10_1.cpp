6870b757df60f218d9ff5fc9358571e55fd7d48c
BossXI <bossweerawut@hotmail.com>
2021-12-11 00:45:28 +0700

    lab10_1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 
 int main(){	
+	float loan,interestperY,pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
+	
 	cout << "Enter interest rate per year (%): ";
+	cin >> interestperY;
+	
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
+	
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +27,30 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+
+	cout << fixed << setprecision(2);
+	float  prevB=loan;
+	int year = 1;
+	do
+	{
+		
+		cout << setw(13) << left << year;
+		cout << setw(13) << left << prevB;
+		float inter = prevB*interestperY/100;
+		cout << setw(13) << left << inter;
+		float total = prevB+inter;
+		cout << setw(13) << left << total;
+		if(total < pay)
+		{
+			pay = total;
+		}
+		cout << setw(13) << left << pay;
+		prevB = total - pay;
+		cout << setw(13) << left <<  prevB ;
+		cout << "\n";	
+		year++;
+	}while(prevB !=0);
+	
 	
 	return 0;
 }
